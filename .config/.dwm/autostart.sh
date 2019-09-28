b82b448894ab4029e88161c208340ec0a35a80ad
archfate <archfate@protonmail.com>
2019-09-28 12:25:47 -1000

    moved dwm autostart into config folder

---
+++ .config/.dwm/autostart.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+setbg &
+sxhkd &
+dwmstatus &
+#feh --randomize --bg-scale ~/Documents/backgrounds/* &
+numlockx &
+compton &
+dunst &
+xautolock -time 10 -locker slock &
+#greenclip daemon &
+redshift &
+
