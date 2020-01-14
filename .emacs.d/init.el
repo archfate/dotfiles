ec97077d869385e4ab797b2e3c2156a99d438e60
archfate <archfate@protonmail.com>
2020-01-13 23:11:03 -1000

    updated

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,4 @@
-
 ;; .emacs.d/init.el
-
 
 ;; ===================================
 
@@ -77,13 +75,37 @@
 (load-theme 'material t)            ;; Load material theme
 
 (global-linum-mode t)               ;; Enable line numbers globally
-
+(setq org-src-tab-acts-natively t)  ;; Indentation for org-mode code block
 ;; ====================================
 ;; Load File
 ;; ====================================
 
-;; Eww linkpiper
+;; Org-Mode
 
+;; ====================================
+
+;; org-mode export markdown
+(eval-after-load "org"
+  '(require 'ox-md nil t))
+(setq org-html-validation-link nil)
+
+;; ====================================
+;; Open files externally
+;; ====================================
+
+(defun prelude-open-with (arg)
+  "Open visited file in default external program.
+
+With a prefix ARG always prompt for command to use."
+  (interactive "P")
+  (when buffer-file-name
+    (shell-command (concat
+                    (cond
+                     ((and (not arg) (eq system-type 'darwin)) "open")
+                     ((and (not arg) (member system-type '(gnu gnu/linux gnu/kfreebsd))) "xdg-open")
+                     (t (read-shell-command "Open current file with: ")))
+                    " "
+                    (shell-quote-argument buffer-file-name)))))
 ;; ====================================
 
 ;; Key and other features
@@ -138,10 +160,11 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (w3m xclip material-theme blacken py-autopep8 flycheck elpy better-defaults))))
+    (flymd w3m xclip material-theme blacken py-autopep8 flycheck elpy better-defaults))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'upcase-region 'disabled nil)
